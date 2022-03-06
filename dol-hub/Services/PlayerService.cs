df1a6adc23c931227ffbcf967544aad9a1b6fbf4
bcolemutech <bcolemutech@hotmail.com>
2022-03-06 16:01:58 -0600

    Player service get and update

--- dol-hub/Services/PlayerService.cs
+++ dol-hub/Services/PlayerService.cs
@@ -1,22 +1,39 @@
 ﻿namespace dol_hub.Services;
 
 using dol_sdk.POCOs;
+using Google.Cloud.Firestore;
+using Microsoft.Extensions.Configuration;
 
 public interface IPlayerService
 {
-    Task<IUser> GetPlayer(string? email);
-    Task<IUser> UpdatePlayer(IUser player);
+    Task<User> GetPlayer(string? userId);
+    Task UpdatePlayer(string userId, IUser? player);
 }
 
 public class PlayerService : IPlayerService
 {
-    public Task<IUser> GetPlayer(string? email)
+    private const string Players = "players";
+    private readonly FirestoreDb _db;
+
+    public PlayerService(IConfiguration configuration)
     {
-        throw new NotImplementedException();
+        _db = FirestoreDb.Create(configuration["ProjectId"]);
     }
 
-    public Task<IUser> UpdatePlayer(IUser player)
+    public async Task<User> GetPlayer(string? userId)
     {
-        throw new NotImplementedException();
+        var docRef = _db.Collection(Players).Document(userId);
+
+        var snapshot = await docRef.GetSnapshotAsync();
+
+        return snapshot.Exists
+            ? snapshot.ConvertTo<User>()
+            : throw new KeyNotFoundException($"No player found with ID: {userId}");
+    }
+
+    public async Task UpdatePlayer(string userId, IUser? player)
+    {
+        var docRef = _db.Collection(Players).Document(userId);
+        await docRef.SetAsync(player, SetOptions.MergeAll);
     }
 }
