d5e8e9fabc453356bc41630128646f00289cf337
bcolemutech <bcolemutech@hotmail.com>
2022-03-06 14:37:17 -0600

    PlayerService.cs

---
+++ dol-hub/Services/PlayerService.cs
@@ -0,0 +1,22 @@
+﻿namespace dol_hub.Services;
+
+using dol_sdk.POCOs;
+
+public interface IPlayerService
+{
+    Task<IUser> GetPlayer(string? email);
+    Task<IUser> UpdatePlayer(IUser player);
+}
+
+public class PlayerService : IPlayerService
+{
+    public Task<IUser> GetPlayer(string? email)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IUser> UpdatePlayer(IUser player)
+    {
+        throw new NotImplementedException();
+    }
+}
