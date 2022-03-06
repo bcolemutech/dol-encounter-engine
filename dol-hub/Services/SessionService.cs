f5c61a9df87eaf8a1c7813c4b9c338e692d616b6
bcolemutech <bcolemutech@hotmail.com>
2022-03-06 14:34:27 -0600

    SessionService.cs

---
+++ dol-hub/Services/SessionService.cs
@@ -0,0 +1,34 @@
+﻿namespace dol_hub.Services;
+
+using dol_sdk.POCOs;
+
+public interface ISessionService
+{
+    Task<ISession?> GetSession(string sessionId);
+    Task AddSession(ISession session);
+    Task EndSession(string sessionId);
+    Task UpdateSession(ISession session);
+}
+
+public class SessionService : ISessionService
+{
+    public Task<ISession?> GetSession(string sessionId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddSession(ISession session)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task EndSession(string sessionId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateSession(ISession session)
+    {
+        throw new NotImplementedException();
+    }
+}
